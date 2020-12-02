358647f97e3412d9798b0f426ba8d03a1d97f410
Matt Raible <matt.raible@okta.com>
2020-12-01 21:58:18 -0700

    Create sample using OktaDev Schematics: ./test-app.sh rn

---
+++ auth.config.js
@@ -0,0 +1,10 @@
+export default {
+  oidc: {
+    clientId: '0oa5nak5fmUbfT3O3357',
+    redirectUri: 'com.okta.dev-133320:/callback',
+    endSessionRedirectUri: 'com.okta.dev-133320:/callback',
+    discoveryUri: 'https://dev-133320.okta.com/oauth2/default',
+    scopes: ['openid', 'profile', 'offline_access'],
+    requireHardwareBackedKeyStore: false,
+  },
+};
