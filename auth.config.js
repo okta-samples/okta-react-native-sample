f0440e60775815834e4f05fcc491cfee59dee6b2
Matt Raible <matt.raible@okta.com>
2020-12-02 10:32:32 -0700

    Use CLI_OKTA_REVERSE_DOMAIN for native

--- auth.config.js
+++ auth.config.js
@@ -1,8 +1,8 @@
 export default {
   oidc: {
     clientId: '${CLI_OKTA_CLIENT_ID}',
-    redirectUri: '${CLI_OKTA_REDIRECT_SCHEME}:/callback',
-    endSessionRedirectUri: '${CLI_OKTA_REDIRECT_SCHEME}:/callback',
+    redirectUri: '${CLI_OKTA_REVERSE_DOMAIN}:/callback',
+    endSessionRedirectUri: '${CLI_OKTA_REVERSE_DOMAIN}:/callback',
     discoveryUri: '${CLI_OKTA_ISSUER}',
     scopes: ['openid', 'profile', 'offline_access'],
     requireHardwareBackedKeyStore: false,
