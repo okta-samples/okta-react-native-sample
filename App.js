f48bf7aa1e4ed72709fdaa3aad889f926c8b7054
Matt Raible <matt.raible@okta.com>
2022-08-23 20:22:09 -0600

    Polish

--- App.js
+++ App.js
@@ -77,7 +77,7 @@ const App: () => Node = () => {
           <Auth />
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits. I'm here!
+            screen and then come back to see your edits.
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
